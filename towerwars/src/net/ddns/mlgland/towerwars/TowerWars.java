84fcebdc4713d9ece4e7f70b6f344ecd18668513
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-04-22 17:48:44 -0400

    make simple code

--- towerwars/src/net/ddns/mlgland/towerwars/TowerWars.java
+++ towerwars/src/net/ddns/mlgland/towerwars/TowerWars.java
@@ -1,17 +1,18 @@
 package net.ddns.mlgland.towerwars;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TowerWars extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[TowerWars] Plugin is now enabled");
     }
 
     @Override
     public void onDisable() {
-
+        getServer().getConsoleSender().sendMessage(ChatColor.RED + "[TowerWars] Plugin is now disabled");
     }
 
 }
