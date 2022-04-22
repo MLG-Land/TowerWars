af2ae6aeeaaf0a3a5598449334c00af1ccf3b7fe
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-04-22 18:06:00 -0400

    make a welcome screen

--- towerwars/src/net/ddns/mlgland/towerwars/TowerWars.java
+++ towerwars/src/net/ddns/mlgland/towerwars/TowerWars.java
@@ -1,5 +1,6 @@
 package net.ddns.mlgland.towerwars;
 
+import net.ddns.mlgland.towerwars.events.Events;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -7,6 +8,10 @@ public class TowerWars extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        getServer().getPluginManager().registerEvents(new Events(), this);
+
+
+
         getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[TowerWars] Plugin is now enabled");
     }
 
